8da8037318b287fd03bd39e5e3d023b780ce3dfc
bal2267 <bal2267@compute-linux1.cs.tamu.edu>
2016-10-06 11:20:59 -0500

    worked on some of the instruction strings

--- Interface.py
+++ Interface.py
@@ -16,9 +16,9 @@ print "Be sure to enter the right parameters!"
 print "--------------------------------------------------------------"
 print "Add User  <Account Number> <Account Name>"
 print "Delete User <Account Number>"
-print "Update Balance <Account Number> <Account(Savings or Checking)> <Money Amount>"
+print "Update Balance <Account Number> <Account(Savings or Checking)> <Money Amount>" 
 print "Display User <Account Number>"
-print "Tansfer Money <Account Number> <Account(Savings or Checking)> <Money Amount>"
+print "Tansfer Money <Account Number> <Account(Savings or Checking)> <Money Amount>" #"transfer <account number> savings <amount>" transfers from savings to checking
 
 error_message = "We didn't quite understand that. Please check your inputs and try again."
 
@@ -32,15 +32,25 @@ def read_in(): #WARNING: when implementing or modifying the recursion in this fu
 		inputsplit = line.split(' ') #splits the input string by whitespace
 		if(inputsplit[0] == "Add"):
 			#modify and send instruction socket to parser
-			pass #tells the compiler to "pass by" without doing anything
+			instr = 'INSERT INTO Users VALUES FROM (' + inputsplit[2] + ', ' + inputsplit[3] + ', 0, 0)' 
+			#					  (account number,        account name,   savings, checking)
+			pass #tells the compiler to "pass by" and ignore without doing anything
+			     #with pass, you can work on a function without having to worry as much about unfinished code
+			     #preventing you from compiling
 		elif(inputsplit[0] == "Delete"):
 			#modify and send instruction socket to parser
 			pass
 		elif(inputsplit[0] == "Update"):
 			#modify and send instruction socket to parser
+			inst1 = 'currentbal <- project (' + inputsplit[3] ') (select (accountNumber == ' + inputsplit[2] + ') Users)' #TODO: how to access this number?
+			#				    savings or checking				     account number
+			#instr = 'UPDATE Users SET (' + inputsplit[3] + ') (' + inputsplit[4] + ') WHERE (accountNumber = ' + inputsplit[2] + ')' #TODO: double check expected format
+			#				account,		amount,						account number
+			
 			pass
 		elif(inputsplit[0] == "Display"):
 			#modify and send instruction socket to parser
+			instr = 'SHOW select (accountNumber == ' + inputsplit[2] + ') Users'
 			pass
 		elif(inputsplit[0] == "Transfer"):
 			#modify and send instruction socket to parser
@@ -48,4 +58,6 @@ def read_in(): #WARNING: when implementing or modifying the recursion in this fu
 		else:
 			print error_message
 
+initialInstruction = "CREATE TABLE Users (accountNumber INTEGER, accountName VARCHAR(30), savings INTEGER, checking INTEGER) PRIMARY KEY (accountName);"
+#TODO: send this initial instruction
 read_in()
