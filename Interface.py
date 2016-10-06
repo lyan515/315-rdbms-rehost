1be53c04a7c8f66eaf99799eb554995271d021ef
bal2267 <bal2267@compute-linux1.cs.tamu.edu>
2016-10-06 14:57:21 -0500

    changed Display, added Exit, and other changes

--- Interface.py
+++ Interface.py
@@ -18,52 +18,64 @@ print "Add User  <Account Number> <Account Name>"
 print "Delete User <Account Number>"
 print "Update Balance <Account Number> <Account(Savings or Checking)> <Money Amount>" 
 print "Display User <Account Number>"
-print "Tansfer Money <Account Number> <Account(Savings or Checking)> <Money Amount>" #"transfer <account number> savings <amount>" transfers from savings to checking
+print "Tansfer Money <Account Number> <Account(Savings or Checking)> <Money Amount>" #what about the target account?
+print "Exit"
 
 error_message = "We didn't quite understand that. Please check your inputs and try again."
 
 #TODO: add socket functionality
 #TODO: comment out all pass statements as the functions are implemented
 
-#TODO: make read_in() return something aftet each inputted line
-def read_in(): #WARNING: when implementing or modifying the recursion in this function, be sure that it doesn't cause too much recursion, which is a thing that causes run time errors.
-	#for line in sys.stdin:
-		instruction = raw_input()
-		inputsplit = line.split(' ') #splits the input string by whitespace
-		if(inputsplit[0] == "Add"):
-			#modify and send instruction socket to parser
-			instr = 'INSERT INTO Users VALUES FROM (' + inputsplit[2] + ', ' + inputsplit[3] + ', 0, 0)' 
-			#					  (account number,        account name,   savings, checking)
-			pass #tells the compiler to "pass by" and ignore without doing anything
-			     #with pass, you can work on a function without having to worry as much about unfinished code
-			     #preventing you from compiling
-		elif(inputsplit[0] == "Delete"):
-			#modify and send instruction socket to parser
-			pass
-		elif(inputsplit[0] == "Update"):
-			#modify and send instruction socket to parser
-			#inst1 = 'currentbal <- project (' + inputsplit[3] ') (select (accountNumber == ' + inputsplit[2] + ') Users)' #TODO: how to access this number?
-			#				    savings or checking				     account number
-			#TODO: check the length of inputsplit[4], which, by default, should contain the amount to update with. however, if the user types, say, "+ amount"
-			#      or "- amount" instead of "+amount" or "-amount". if this has happened, inputsplit[4] will be of length 1, and inputsplit[5] will exist and
-			#      will contain the actual amount
-			#      alternatively, we just tell the user not to put a space between the +/- and the amount. I personally like this idea better
+#TODO: make read_in() return something after each inputted line
 
-			#newNumber = number obtained from database + (or -) inputted amount (inputsplit[4 or 5])
+def read_in():
+	instruction = raw_input()
+	inputsplit = line.split(' ') #splits the input string by whitespace
+	if(inputsplit[0] == "Add"):
+		#modify and send instruction socket to parser
+		instr = 'INSERT INTO Users VALUES FROM (' + inputsplit[2] + ', ' + inputsplit[3] + ', 0, 0);' 
+		#					  (account number,        account name,   savings, checking)
 
-			#instr = 'UPDATE Users SET (' + inputsplit[3] + ') (' + newNumber + ') WHERE (accountNumber = ' + inputsplit[2] + ')' #TODO: double check expected format
-			#				account,		amount,						account number
-			
-			pass
-		elif(inputsplit[0] == "Display"):
-			#modify and send instruction socket to parser
-			instr = 'SHOW select (accountNumber == ' + inputsplit[2] + ') Users'
-			pass
-		elif(inputsplit[0] == "Transfer"):
-			#modify and send instruction socket to parser
-			pass
-		else:
-			print error_message
+		pass #tells the compiler to "pass by" and ignore without doing anything
+		     #with pass, you can work on a function without having to worry as much about unfinished code
+		     #preventing you from compiling
+	elif(inputsplit[0] == "Delete"):
+		#modify and send instruction socket to parser
+
+		instr = 'DELETE FROM Users WHERE (accountNumber == ' + inputsplit[2] + ');'
+
+		pass
+	elif(inputsplit[0] == "Update"):
+		#modify and send instructions socket to parser
+		#inst1 = 'currentbal <- project (' + inputsplit[3] ') (select (accountNumber == ' + inputsplit[2] + ') Users);' #TODO: how to access this number?
+		#				    savings or checking				     account number
+
+		#TODO: check the length of inputsplit[4], which, by default, should contain the amount to update with. however, if the user types, say, "+ amount"
+		#      or "- amount" instead of "+amount" or "-amount". if this has happened, inputsplit[4] will be of length 1, and inputsplit[5] will exist and
+		#      will contain the actual amount
+		#      alternatively, we just tell the user not to put a space between the +/- and the amount. I personally like this idea better
+
+		#newNumber = number obtained from database + (or -) inputted amount (inputsplit[4 or 5])
+
+		#instr = 'UPDATE Users SET (' + inputsplit[3] + ') (' + newNumber + ') WHERE (accountNumber = ' + inputsplit[2] + ');' #TODO: double check expected format
+		#				account,		amount,						account number
+		
+		pass
+	elif(inputsplit[0] == "Display"):
+		#modify and send instructions socket to parser
+		instr1 = 'thisUser <- select(accountNumber == ' + inputsplit[2] ') Users;'	#INSERT is the only command that can handle query inputs
+		instr2 = 'SHOW thisUser;'							#splitting Display into these three instructions gets around this issue
+		instr3 = 'DROP TABLE thisUser;' #allows us to reuse thisUser			#without us having to restructure a significant amount of our code
+
+		pass
+	elif(inputsplit[0] == "Transfer"):
+		#modify and send instruction socket to parser
+		pass
+	elif(inputsplit[0] == "Exit"
+		instr = 'EXIT;'
+		pass
+	else:
+		print error_message
 
 initialInstruction = "CREATE TABLE Users (accountNumber INTEGER, accountName VARCHAR(30), savings INTEGER, checking INTEGER) PRIMARY KEY (accountName);"
 #TODO: send this initial instruction
