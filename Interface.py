c556eb36c519dc8e3c1c3a131fb568a52a1b4dca
bal2267 <bal2267@compute-linux1.cs.tamu.edu>
2016-10-06 22:42:40 -0500

    added Send instruction, and made notes for all implementations.

--- Interface.py
+++ Interface.py
@@ -16,21 +16,18 @@ print "Be sure to enter the right parameters!"
 print "--------------------------------------------------------------"
 print "Add User  <Account Number> <Account Name>"
 print "Delete User <Account Number>"
-print "Update Balance <Account Number> <Account(Savings or Checking)> <Money Amount>" 
+print "Update Balance <Account Number> <Account(Savings or Checking)> <Money Amount>    (put money to subtract in parenthesis like this: '(12345)' "
+#	TODO: make this look nicer 
 print "Display User <Account Number>"
-<<<<<<< HEAD
-print "Tansfer Money <Account Number> <Account(Savings or Checking)> <Money Amount>" #what about the target account?
-=======
-print "Tansfer Money <Account Number> <Account(Savings or Checking)> <Money Amount>"
->>>>>>> master
+print "Tansfer Money <Account Number> <Account(Savings or Checking)> <Money Amount>" 
+#	from input account to other option (savings to checking or vice versa)
+print "Send Money <Source Account Number> <Account(Savings or Checking)> <Money Amount> <Recipient Account Number>"
 print "Exit"
 
 error_message = "We didn't quite understand that. Please check your inputs and try again."
 
 #TODO: add socket functionality
 #TODO: comment out all pass statements as the functions are implemented
-
-<<<<<<< HEAD
 #TODO: make read_in() return something after each inputted line
 
 def read_in():
@@ -40,7 +37,9 @@ def read_in():
 		#modify and send instruction socket to parser
 		instr = 'INSERT INTO Users VALUES FROM (' + inputsplit[2] + ', ' + inputsplit[3] + ', 0, 0);' 
 		#					  (account number,        account name,   savings, checking)
-
+		
+		#read_in()
+		
 		pass #tells the compiler to "pass by" and ignore without doing anything
 		     #with pass, you can work on a function without having to worry as much about unfinished code
 		     #preventing you from compiling
@@ -50,22 +49,27 @@ def read_in():
 		instr = 'DELETE FROM Users WHERE (accountNumber == ' + inputsplit[2] + ');'
 
 		pass
+		#read_in()
 	elif(inputsplit[0] == "Update"):
 		#modify and send instructions socket to parser
-		#inst1 = 'currentbal <- project (' + inputsplit[3] ') (select (accountNumber == ' + inputsplit[2] + ') Users);' #TODO: how to access this number?
+		#inst1 = 'currentbal <- project (' + inputsplit[3] ') (select (accountNumber == ' + inputsplit[2] + ') Users);' #TODO: store this result in a variable
 		#				    savings or checking				     account number
-
-		#TODO: check the length of inputsplit[4], which, by default, should contain the amount to update with. however, if the user types, say, "+ amount"
-		#      or "- amount" instead of "+amount" or "-amount". if this has happened, inputsplit[4] will be of length 1, and inputsplit[5] will exist and
-		#      will contain the actual amount
-		#      alternatively, we just tell the user not to put a space between the +/- and the amount. I personally like this idea better
-
-		#newNumber = number obtained from database + (or -) inputted amount (inputsplit[4 or 5])
-
-		#instr = 'UPDATE Users SET (' + inputsplit[3] + ') (' + newNumber + ') WHERE (accountNumber = ' + inputsplit[2] + ');' #TODO: double check expected format
+		
+		#if('(' in inputsplit[4]):
+		#	if(')' in inputsplit[4]):
+		#		newNumber = number obtained from database - inputted amount (inputsplit[4])
+		#	else:
+		#		print error_message
+		#elif(')' not in inputsplit[4]):
+		#	newNumber = number obtained from database + inputted amount (inputsplit[4])
+		#else:
+		#	print error_message
+		
+		#instr = 'UPDATE Users SET (' + inputsplit[3] + ') (' + newNumber + ') WHERE (accountNumber = ' + inputsplit[2] + ');'
 		#				account,		amount,						account number
 		
 		pass
+		#read_in()
 	elif(inputsplit[0] == "Display"):
 		#modify and send instructions socket to parser
 		instr1 = 'thisUser <- select(accountNumber == ' + inputsplit[2] ') Users;'	#INSERT is the only command that can handle query inputs
@@ -73,47 +77,68 @@ def read_in():
 		instr3 = 'DROP TABLE thisUser;' #allows us to reuse thisUser			#without us having to restructure a significant amount of our code
 
 		pass
+		#read_in()
 	elif(inputsplit[0] == "Transfer"):
 		#modify and send instruction socket to parser
+		
+		#inst1 = 'currentbal <- project (' + inputsplit[3] ') (select (accountNumber == ' + inputsplit[2] + ') Users);' #TODO: store this result in a variable
+                #                                   savings or checking                              account number
+
+	        #if(inst1 < int(inputsplit[4])):
+		#	print 'not enough funds in ' + inputsplit[3]	
+
+		#if(inputsplit[3] == 'Savings'):
+		#	otherAccount = 'Checking')
+		#elif(inputsplit[3] == 'Checking'):
+		#	otherAccount = 'Savings')
+		#else:
+		#	print error_message
+
+		#inst2 = 'currentbal <- project (' + otherAccount ') (select (accountNumber == ' + inputsplit[2] + ') Users);' #TODO: store this result in a variable
+                #                                                                                account number
+
+		#subtraction = inst1 - int(inputsplit[4])
+		#addition = inst2 + int(inputsplit[4])
+
+		#instr3 = 'UPDATE Users SET (' + inputsplit[3] + ') (' + subtraction + ') WHERE (accountNumber = ' + inputsplit[2] + ');'
+                #                               account,                 amount,                                         account number
+
+		#instr4 = 'UPDATE Users SET (' + otherAccount + ') (' + addition + ') WHERE (accountNumber = ' + inputsplit[2] + ');'
+                #                               account,               amount,                                  account number
+
 		pass
+		#read_in()
+	elif(inputsplit[0] == "Send"):
+
+		#inst1 = 'currentbal <- project (' + inputsplit[3] ') (select (accountNumber == ' + inputsplit[2] + ') Users);' #TODO: store this result in a variable
+                #                                   savings or checking                              account number
+
+		#if(inst1 < int(inputsplit[4])):
+                #       print 'not enough funds in ' + inputsplit[3]   
+
+		#inst2 = 'currentbal <- project (Checking) (select (accountNumber == ' + inputsplit[5] + ') Users);' #TODO: store this result in a variable
+                #                               transfers go to checking by default       target account number
+
+		#subtraction = inst1 - int(inputsplit[4])
+                #addition = inst2 + int(inputsplit[4])
+
+                #instr3 = 'UPDATE Users SET (' + inputsplit[3] + ') (' + subtraction + ') WHERE (accountNumber = ' + inputsplit[2] + ');'
+                #                               account,                 amount,                                     account number
+
+		#instr4 = 'UPDATE Users SET (Checking) (' + addition + ') WHERE (accountNumber = ' + inputsplit[5] + ');'
+                #                            account,       amount,                                  target account number
+
+		pass
+		#read_in()
 	elif(inputsplit[0] == "Exit"
 		instr = 'EXIT;'
 		pass
+		#read_in()
 	else:
 		print error_message
 
 =======
-#TODO: make read_in() return something aftet each inputted line
-def read_in(): 
-    while (exit == 0):
-	#for line in sys.stdin:
-		instruction = raw_input() #not necessary, but I think it looks nicer
-		inputsplit = line.split(' ') #splits the input string by whitespace
-		if(inputsplit[0] == "Add"):
-			#Add User <Account Number> <Account Name>
-			#modify and send instruction socket to parser
-			instruction =  "INSERT INTO Users" + inputsplit[2] + ";" #other stuff
-			pass #tells the compiler to "pass by" without doing anything
-		elif(inputsplit[0] == "Delete"):
-			#modify and send instruction socket to parser
-			instruction = "DELETE FROM Users WHERE accountNumber == " + inputsplit[2]
-			pass
-		elif(inputsplit[0] == "Update"):
-			#modify and send instruction socket to parser
-			pass
-		elif(inputsplit[0] == "Display"):
-			#modify and send instruction socket to parser
-			pass
-		elif(inputsplit[0] == "Transfer"):
-			#modify and send instruction socket to parser
-			pass
-		elif(inputsplit[0] == "Exit"):
-			instruction = "EXIT;"
-			pass
-		else:
-			print error_message
-
->>>>>>> master
+#TODO: make read_in() return something after each inputted line
 initialInstruction = "CREATE TABLE Users (accountNumber INTEGER, accountName VARCHAR(30), savings INTEGER, checking INTEGER) PRIMARY KEY (accountName);"
 #TODO: send this initial instruction
 read_in()
